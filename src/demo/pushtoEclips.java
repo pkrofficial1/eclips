06eac5d36cd6d138dd27b203fa545a21ed3026fe
pkrofficial1 <www.pkrofficial@gmail.com>
2024-06-10 11:54:31 +0530

    modify

--- src/demo/pushtoEclips.java
+++ src/demo/pushtoEclips.java
@@ -3,7 +3,7 @@ package demo;
 public class pushtoEclips {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(" completed successfully.");
 
 	}
 
