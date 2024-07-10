28a94da0aafa4acb51bcac9e6319bfb0f6ad34b0
Praveen Kumar M <www.pkrofficial@gmail.com>
2024-07-10 09:54:16 +0530

    Demo.java

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println(" completed successfully.");
+		System.out.println(" collabrators ");
+
+	}
+
+}
